e5a0b4a6ac15d066c743a66c1e4f97ceb3d21953
vaidehi2701 <vaidehi2701@gmail.com>
2019-11-18 17:03:07 +0530

    intro slider added

--- lib/login_signup/intro_slider.dart
+++ lib/login_signup/intro_slider.dart
@@ -7,8 +7,11 @@ class IntroSlider extends StatelessWidget {
     PageViewModel(
         pageColor: Colors.lightBlue[400],
         bubble: Image.asset('images/logo.png'),
-        body: Text(
-          'Fill Up simple Form and do register As Freelancer and get a job.',
+        body: Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: Text(
+            'Fill Up simple Form and do register As Freelancer and get a job.',
+          ),
         ),
         title: Padding(
           padding: const EdgeInsets.only(top: 55),
@@ -33,10 +36,16 @@ class IntroSlider extends StatelessWidget {
     PageViewModel(
       pageColor: Colors.orange[400],
       iconImageAssetPath: 'images/logo.png',
-      body: Text(
-        'Fill Up all necessary Details and get a automated genrated Resume that will send to client for their Job Requirement.',
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: Text(
+          'Fill Up all necessary Details and get a automated genrated Resume that will send to client for their Job Requirement.',
+        ),
       ),
-      title: Text('Automatic Resume'),
+      title: Padding(
+        padding: const EdgeInsets.only(top: 55),
+        child: Text('Automatic Resume'),
+      ),
       mainImage: Image.asset(
         'images/resume.png',
         height: 140.0,
@@ -55,10 +64,16 @@ class IntroSlider extends StatelessWidget {
     PageViewModel(
       pageColor: Colors.blueGrey[400],
       iconImageAssetPath: 'images/logo.png',
-      body: Text(
-        'You Can Find Number Of Jobs  with different categories and options and also u can apply directly with this app',
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: Text(
+          'You Can Find Number Of Jobs  with different categories and options and also u can apply directly with this app',
+        ),
       ),
-      title: Text('Find job'),
+      title: Padding(
+        padding: const EdgeInsets.only(top: 55),
+        child: Text('Find job'),
+      ),
       mainImage: Image.asset(
         'images/list_job.png',
         height: 170.0,
